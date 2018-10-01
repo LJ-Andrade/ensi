f3478d7fb016fa88713bb0e3edb3e4ea8e26d212
javzero <javzero1@gmail.com>
2018-10-01 10:45:24 -0300

    Rubros

--- includes/foot.php
+++ includes/foot.php
@@ -1,8 +1,8 @@
         <!-- Js -->
-        <script src="vendor/js/vendor/jquery-2.1.1.min.js"></script>
+        <script src="vendor/js/jquery-2.1.1.min.js"></script>
         <script src="vendor/boostrap/js/theter.min.js"></script>
         <script src="vendor/boostrap/js/bootstrap.min.js"></script>
-        <script src="vendor/js/vendor/modernizr-2.6.2.min.js"></script>
+        <script src="vendor/js/modernizr-2.6.2.min.js"></script>
 		<script src="https://unpkg.com/ionicons@4.1.2/dist/ionicons.js"></script>
         <script src="vendor/owl-carousel/owl.carousel.min.js"></script>
         <script src="vendor/js/jquery.validate.min.js"></script>
--- rubros-especializados.php
+++ rubros-especializados.php
@@ -12,23 +12,115 @@
 		<!--
         Company
         ==================== -->
-		<section id="quienessomos" class="service section company-section">
+		<section id="quienessomos" class="service section rubros">
 			<div class="container">
-				<div class="row wow fadeIn" data-wow-delay="0.3s">
+				<div class="row" data-wow-delay="0.3s">
 					<h2>RUBROS ESPECIALIZADOS</h2>
-					<hr>
-					<div class="col-md-3"><img src="images/clients/1.png"></div>
-          <div class="col-md-3"><img src="images/clients/2.png"></div>
-          <div class="col-md-3"><img src="images/clients/3.png"></div>
-          <div class="col-md-3"><img src="images/clients/4.png"></div>
-          <div class="col-md-3"><img src="images/clients/2.png"></div>
-          <div class="col-md-3"><img src="images/clients/4.png"></div>
-          <div class="col-md-3"><img src="images/clients/1.png"></div>
-          <div class="col-md-3"><img src="images/clients/2.png"></div>
-          <div class="col-md-3"><img src="images/clients/3.png"></div>
-          <div class="col-md-3"><img src="images/clients/1.png"></div>
-          <div class="col-md-3"><img src="images/clients/3.png"></div>
-          <div class="col-md-3"><img src="images/clients/4.png"></div>
+                    <br>
+                    <div class="row img-and-text-items wow fadeIn">
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/quimicos-farmaceuticos.jpg">
+                        </div>
+                        <div class="col-md-6 text">
+                            <h1>QUÍMICOS <br> FARMACÉUTICOS</h1>
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items wow fadeIn">
+                        <div class="col-md-6 text">
+                            <h1>QUÍMICOS <br> INDUSTRIALES</h1>
+                        </div>
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/quimicos-industriales.jpg">
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/estudios-clinicos.jpg">
+                        </div>
+                        <div class="col-md-6 text">
+                            <h1>ESTUDIOS <br> CLÍNICOS</h1>
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 text">
+                            <h1>COLORANTES  <br> y  TINTAS</h1>
+                        </div>
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/colorantes-y-tintas.jpg">
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/semillas-y-granos.jpg">
+                        </div>
+                        <div class="col-md-6 text">
+                            <h1>SEMILLAS <br>  Y  GRANOS</h1>
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 text">
+                            <h1>INDUSTRIA DEL PAPEL <br>y CARTÓN</h1>
+                        </div>
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/industria-del-papel-y-carton.jpg">
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/transformadores.jpg">
+                        </div>
+                        <div class="col-md-6 text  wow fadeIn">
+                            <h1>TRANSFORMADORES</h1>
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 text">
+                            <h1>POLIURETANOS</h1>
+                        </div>
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/polieuretanos.jpg">
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/maquinarias-y-repuestos.jpg">
+                        </div>
+                        <div class="col-md-6 text">
+                            <h1>MAQUINARIAS <br> y  REPUESTOS</h1>
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 text">
+                            <h1> INDUSTRIA DE ENVASES <br> Y PAQUETERÍA</h1>
+                        </div>
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/industria-de-envases-paqueteria.jpg">
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/electronica-e-impresoras.jpg">
+                        </div>
+                        <div class="col-md-6 text">
+                            <h1>ELECTRÓNICA, <br> IMPRESORAS</h1>
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 text">
+                            <h1>CENTROS DE ESQUÍ  <br> y  RECREACIÓN DE MONTAÑA</h1>
+                        </div>
+                        <div class="col-md-6 image">
+                            <img src="images/rubros/centros-de-esqui-y-recreacion-de-montana.jpg">
+                        </div>
+                    </div>
+                    <div class="row img-and-text-items  wow fadeIn">
+                        <div class="col-md-6 col-xs-6 image">
+                            <img src="images/rubros/cosmeticos.jpg">
+                        </div>
+                        <div class="col-md-6 col-xs-6 text">
+                            <h1>COSMÉTICOS</h1>
+                        </div>
+                    </div>
 				</div>
 			</div><!-- .container close -->
 		</section><!-- #service close -->
